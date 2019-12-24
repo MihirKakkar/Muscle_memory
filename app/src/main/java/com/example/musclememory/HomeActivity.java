059d60afb214a0595d852cb0eb6a73e434ecbbb2
MihirKakkar <42092170+MihirKakkar@users.noreply.github.com>
2019-12-24 00:05:46 -0500

    adding button to add muscles

--- app/src/main/java/com/example/musclememory/HomeActivity.java
+++ app/src/main/java/com/example/musclememory/HomeActivity.java
@@ -3,6 +3,8 @@ package com.example.musclememory;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -10,5 +12,13 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        Button adder = (Button) findViewById(R.id.bodyadder);
+        adder.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
