6ac5322c16418aa7554213145ff83446339d46bc
MihirKakkar <42092170+MihirKakkar@users.noreply.github.com>
2019-12-23 21:10:45 -0500

    done for monday

--- app/src/main/java/com/example/musclememory/MainActivity.java
+++ app/src/main/java/com/example/musclememory/MainActivity.java
@@ -1,22 +1,13 @@
 package com.example.musclememory;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.content.Intent;
 import android.os.Bundle;
-
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-
 import android.os.Handler;
-import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
-
-    private static int TIMER_FOR_OVERLAY = 4000;
+    private static int OVERLAYTIMEOUT = 4000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +20,6 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(homeIntent);
                 finish();
             }
-        }, TIMER_FOR_OVERLAY);
+        },OVERLAYTIMEOUT);
     }
 }
