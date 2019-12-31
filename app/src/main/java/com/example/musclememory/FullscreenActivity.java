5b388d45ae77d4008b427fc96cb5d80f57ccdda1
MihirKakkar <42092170+MihirKakkar@users.noreply.github.com>
2019-12-31 01:56:57 -0500

    last one

--- app/src/main/java/com/example/musclememory/FullscreenActivity.java
+++ app/src/main/java/com/example/musclememory/FullscreenActivity.java
@@ -3,6 +3,8 @@ package com.example.musclememory;
 import android.annotation.SuppressLint;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.MotionEvent;
